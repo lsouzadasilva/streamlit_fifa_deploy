eda8f3c7d5e816e5f33bc8c8ebbfd4a7a805bafe
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-07 23:55:34 -0300

    Update 1_🏚️_home.py

--- 1_🏚️_home.py
+++ 1_🏚️_home.py
@@ -22,7 +22,7 @@ if "data" not in st.session_state:
 
 
 st.markdown("# FIFA23 OFFICIAL DATASET!⚽")
-st.markdown("""
+st.sidebar.markdown("""
     **Desenvolvido por Leandro Souza**  
     [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
     [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/streamlit_relatorio_fifa23/)
